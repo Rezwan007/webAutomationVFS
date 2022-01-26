f84405d4f2c3a112b6632c31b6f626b3fb865802
Rezwan <utsha1234@gmail.com>
2022-01-26 18:13:51 +0600

    Update of some work

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='webAutomationVFS',
+    version='1.0.0.0.0',
+    packages=[''],
+    url='www.facebook.com/hasane.rezwan',
+    license='',
+    author='Rezwan',
+    author_email='utsha1234@gmail.com',
+    description='Web Automation'
+)
